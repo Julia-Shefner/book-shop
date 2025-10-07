fccd4f83e5fd58a85d23d818ee1fa8c5b48579f8
Yuliia_Shefner <yul.she91@gmail.com>
2025-10-08 00:22:57 +0200

    initial, setting up

---
+++ src/js/hero.js
@@ -0,0 +1,6 @@
+// "import 'swiper/css';" потрібно додатково додати в файл js після "import Swiper from 'swiper';"
+// або
+// "import 'swiper/css'; import 'swiper/css/navigation';
+//  import 'swiper/css/pagination';"
+// потрібно додатково додати в файл js після "import Swiper from 'swiper'; import { Navigation, Pagination } from 'swiper/modules';"
+// потрібно читати документацію, який саме варіант потрібен
---
+++ src/js/modal-book.js
@@ -0,0 +1,1 @@
+// "import "accordion-js/dist/accordion.min.css";" потрібно додатково додати в файл js після "import Accordion from "accordion-js";"
