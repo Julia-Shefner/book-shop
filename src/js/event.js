42a195dbcb9a5444f6cf40a63c51758428b4a55d
Yuliia_Shefner <yul.she91@gmail.com>
2025-10-11 16:27:48 +0200

    update packege.json

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,35 @@
-// "import 'swiper/css';" потрібно додатково додати в файл js після "import Swiper from 'swiper';"
-// або
-// "import 'swiper/css'; import 'swiper/css/navigation';
-//  import 'swiper/css/pagination';"
-// потрібно додатково додати в файл js після "import Swiper from 'swiper'; import { Navigation, Pagination } from 'swiper/modules';"
-// потрібно читати документацію, який саме варіант потрібен
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Keyboard } from 'swiper/modules';
+
+let eventsSwiper;
+
+function swiperInit() {
+  if (window.innerWidth < 1440) {
+    eventsSwiper = new Swiper('.events-swiper', {
+      modules: [Navigation, Pagination, Keyboard],
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+      breakpoints: {
+        375: { slidesPerView: 1 },
+        768: { slidesPerView: 2, spaceBetween: 24 },
+      },
+    });
+  } else {
+    eventsSwiper.destroy(true, true);
+    eventsSwiper = null;
+  }
+}
+
+swiperInit();
